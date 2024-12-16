9d29a6376263ecccf79b009248bedcdcf01519c1
justseia <seiitmurat.bekmuratuly@gmail.com>
2024-12-16 15:32:28 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/healvoyage/healvoyage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.healvoyage.healvoyage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/webview.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            print('onProgress');
+            print(progress);
+          },
+          onPageStarted: (String url) {
+            print('onPageStarted');
+          },
+          onPageFinished: (String url) {
+            print('onPageFinished');
+          },
+          onWebResourceError: (WebResourceError error) {
+            print('onWebResourceError');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://flutter.dev')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
